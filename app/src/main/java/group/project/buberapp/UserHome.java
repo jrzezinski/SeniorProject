6badbb9c436033cbf51218f944fcccbc324d20ae
jrzezinski <jrzezinski@yahoo.com>
2019-06-08 21:10:53 -0400

    Moved map from UserHome activity into fragment

--- app/src/main/java/group/project/buberapp/UserHome.java
+++ app/src/main/java/group/project/buberapp/UserHome.java
@@ -33,11 +33,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserHome extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
+public class UserHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
-    private GoogleMap mMap;
-    private EditText searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +43,6 @@ public class UserHome extends AppCompatActivity implements OnMapReadyCallback, N
         setContentView(R.layout.activity_user_home);
 
         // initialize variables
-        searchText = findViewById(R.id.search_text);
         drawer = findViewById(R.id.user_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -54,23 +51,20 @@ public class UserHome extends AppCompatActivity implements OnMapReadyCallback, N
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Add top left nav drawer toggle
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        //mapFragment.getMapAsync(this);
-
         // open map fragment initially not on rotate
         if(savedInstanceState == null)
         {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFragment()).commit();
             navigationView.setCheckedItem(R.id.map_view);
         }
+
+        // Add top left nav drawer toggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
+    // Redirect navigation item selection to correct fragment
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
     {
@@ -107,78 +101,5 @@ public class UserHome extends AppCompatActivity implements OnMapReadyCallback, N
         {
             super.onBackPressed();
         }
-    }
-
-    private void initMapSearch()
-    {
-        // catch user enter for searchText input
-        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener()
-                                                {
-                                                    @Override
-                                                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
-                                                    {
-                                                        if(actionId == EditorInfo.IME_ACTION_SEARCH
-                                                        || actionId == EditorInfo.IME_ACTION_DONE
-                                                        || event.getAction() == KeyEvent.ACTION_DOWN
-                                                        || event.getAction() == KeyEvent.KEYCODE_ENTER)
-                                                        {
-                                                            // run search function
-                                                            searchLocation();
-                                                        }
-
-                                                        return false;
-                                                    }
-                                                });
-    }
-
-    // search user input location on the map
-    private void searchLocation()
-    {
-        // user text
-        String searchString = searchText.getText().toString();
-
-        // init geolocation and get user text address lat and long
-        Geocoder geocoder = new Geocoder(UserHome.this);
-        List<Address> list = new ArrayList<>();
-        try
-        {
-            list = geocoder.getFromLocationName(searchString, 1);
-        }catch (IOException e)
-        {
-
-        }
-
-        // if there is at least one address get the first
-        if(list.size() > 0)
-        {
-            // get lat and long info and map marker
-            Address address = list.get(0);
-            LatLng location = new LatLng(address.getLatitude(), address.getLongitude());
-            MarkerOptions options = new MarkerOptions().position(location);
-
-            // move camera to lat and long and set pin
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15.0f));
-            mMap.addMarker(options);
-
-            // hide keyboard
-            this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
-        }
-    }
-
-    // Google maps added method for map load response
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // zoom functionality
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.getUiSettings().setAllGesturesEnabled(true);
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-        initMapSearch();
     }
 }
