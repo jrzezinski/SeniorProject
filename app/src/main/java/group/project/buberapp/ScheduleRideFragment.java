278713e56559debc851e3d9607ecb02fe974ee4b
jrzezinski <jrzezinski@yahoo.com>
2019-06-09 00:31:41 -0400

    Added schedule ride fragment design and basic functions

--- app/src/main/java/group/project/buberapp/ScheduleRideFragment.java
+++ app/src/main/java/group/project/buberapp/ScheduleRideFragment.java
@@ -8,11 +8,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ScheduleRideFragment extends Fragment
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ScheduleRideFragment extends Fragment implements OnMapReadyCallback
 {
+    private GoogleMap mMap;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_schedule_ride, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        // initialize variables
+        View view = inflater.inflate(R.layout.fragment_schedule_ride, container, false);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map_schedule);
+        mapFragment.getMapAsync(this);
+
+        return view;
+    }
+
+    // Google maps added method for map load response
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // zoom functionality
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15.0f));
     }
 }
