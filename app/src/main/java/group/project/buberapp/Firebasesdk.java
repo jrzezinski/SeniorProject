1b4685b5d96e46fe6a78bd7a8d445db3fb475855
yohrmund <51220948+yohrmund@users.noreply.github.com>
2019-06-12 19:58:26 -0400

    Delete Firebasesdk.java

--- app/src/main/java/group/project/buberapp/Firebasesdk.java
+++
@@ -1,8 +0,0 @@
- // Initializing Firebase SDK 
- 
- FirebaseOptions options = new FirebaseOptions.Builder()
-    .setCredentials(GoogleCredentials.getApplicationDefault())
-    .setDatabaseUrl("https://seniorproject-master.firebaseio.com/")
-    .build();
-
-FirebaseApp.initializeApp(options); 
