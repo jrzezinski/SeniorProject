b8cb63d7e97d0a17a8fd48f6ef4362879342fe22
cprice90 <cprice90@gmail.com>
2019-06-14 18:47:58 -0400

    Adding my own work

--- app/src/main/java/group/project/buberapp/HelpFragment.java
+++ app/src/main/java/group/project/buberapp/HelpFragment.java
@@ -8,11 +8,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class HelpFragment extends Fragment
 {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_help, container, false);
+    }
+}
+
+
+public class HelpItem {
+    private String mTitle;
+    private String mText;
+
+    public HelpItem(String title, String text) {
+        mTitle = title;
+        mText = text;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    private static int lastHelpItemId = 0;
+    public static ArrayList<HelpItem> createHelpItemsList(int numHelpItems) {
+        ArrayList<HelpItem> helpItems = new ArrayList<HelpItem>();
+//  WIP
+//        for (int i = 1; i <= numHelpItems; i++) {
+//            helpItems.add(new HelpItem("Help Item (Person) " + ++lastHelpItemId, i <= numHelpItems / 2));
+//        }
+
+        return helpItems;
     }
 }
--- app/src/main/java/group/project/buberapp/RideHistoryFragment.java
+++ app/src/main/java/group/project/buberapp/RideHistoryFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class RideHistoryFragment extends Fragment
 {
     @Nullable
