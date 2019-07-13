040c39d5eaf7095be104f7d1ce491ab4b7e4cd01
jrzezinski <jrzezinski@yahoo.com>
2019-07-12 21:47:01 -0400

    adding mini map into current ride/job selection fragment (not functional only displays map)

--- app/src/main/java/group/project/buberapp/JobCard.java
+++ app/src/main/java/group/project/buberapp/JobCard.java
@@ -2,22 +2,28 @@ package group.project.buberapp;
 
 import com.google.firebase.Timestamp;
 
+import java.util.Map;
+
 public class JobCard
 {
     private int rideTime;
     private Timestamp pickupTime;
     private int payout;
+    private Map<Integer, Integer> locationStart;
+    private Map<Integer, Integer> locationEnd;
 
     public JobCard()
     {
         // empty constructor needed
     }
 
-    public JobCard(int rideTime, Timestamp pickupTime, int payout)
+    public JobCard(int rideTime, Timestamp pickupTime, int payout, Map<Integer, Integer> locationStart, Map<Integer, Integer> locationEnd)
     {
         this.rideTime = rideTime;
         this.pickupTime = pickupTime;
         this.payout = payout;
+        this.locationStart = locationStart;
+        this.locationEnd = locationEnd;
     }
 
     public int getRideTime()
@@ -34,4 +40,8 @@ public class JobCard
     {
         return payout;
     }
+
+    public Map<Integer, Integer> getLocationStart() { return locationStart; }
+
+    public Map<Integer, Integer> getLocationEnd() { return locationEnd; }
 }
--- app/src/main/java/group/project/buberapp/JobCardAdapter.java
+++ app/src/main/java/group/project/buberapp/JobCardAdapter.java
@@ -1,22 +1,39 @@
 package group.project.buberapp;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Map;
+
 public class JobCardAdapter extends FirestoreRecyclerAdapter<JobCard, JobCardAdapter.JobHolder>
 {
     private onItemClickListener listener;
+
+    private Map<Integer, Integer> startLoc;
+    private Map<Integer, Integer> endLoc;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference jobList = db.collection("Rides");
@@ -31,6 +48,53 @@ public class JobCardAdapter extends FirestoreRecyclerAdapter<JobCard, JobCardAda
         holder.textViewRideTime.setText(String.valueOf(model.getRideTime()));
         holder.textViewPickupTime.setText(model.getPickupTime().toDate().toString());
         holder.textViewPayout.setText(String.valueOf(model.getPayout()));
+        startLoc = model.getLocationStart();
+        startLoc = model.getLocationEnd();
+
+        GoogleMap tripMap = holder.tripMap;
+
+        if (tripMap != null)
+        {
+            double startLat = 28.056999 ;
+            double startLong = -82.425987;
+            double endLat = 28.056999;
+            double endLong = -82.425987;
+
+            if (startLoc != null)
+            {
+                startLat = startLoc.get("latitude");
+                startLong = startLoc.get("longitude");
+            }
+
+            if (endLoc != null)
+            {
+                endLat = endLoc.get("latitude");
+                endLong = endLoc.get("longitude");
+            }
+
+            LatLng currentLocation = new LatLng(startLat, startLong);
+            LatLng location = new LatLng(endLat, endLong);
+
+            MarkerOptions optionsStart = new MarkerOptions().position(currentLocation).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+            MarkerOptions optionsEnd = new MarkerOptions().position(new LatLng(endLat, endLong));
+
+            tripMap.addMarker(optionsStart);
+            tripMap.addMarker(optionsEnd);
+
+            // create bounds od the map markers (north, south, ...)
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(location);
+            builder.include(currentLocation);
+            LatLngBounds bounds = builder.build();
+
+            // create the padding to keep markers away from map edge
+            int width = holder.mapView.getContext().getResources().getDisplayMetrics().widthPixels;
+            int height = holder.mapView.getContext().getResources().getDisplayMetrics().heightPixels;
+            int padding = (int) (height * 0.20);
+
+            // move the camera too the correct zoom for the points
+            tripMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding));
+        }
     }
 
     @NonNull
@@ -41,11 +105,14 @@ public class JobCardAdapter extends FirestoreRecyclerAdapter<JobCard, JobCardAda
         return new JobHolder(view);
     }
 
-    class JobHolder extends RecyclerView.ViewHolder
+    class JobHolder extends RecyclerView.ViewHolder implements OnMapReadyCallback
     {
         TextView textViewRideTime;
         TextView textViewPickupTime;
         TextView textViewPayout;
+
+        GoogleMap tripMap;
+        MapView mapView;
 
         public JobHolder(final View itemView)
         {
@@ -53,6 +120,8 @@ public class JobCardAdapter extends FirestoreRecyclerAdapter<JobCard, JobCardAda
             textViewRideTime = itemView.findViewById(R.id.ride_time);
             textViewPickupTime = itemView.findViewById(R.id.pickup_time);
             textViewPayout = itemView.findViewById(R.id.payout);
+
+            mapView = (MapView) itemView.findViewById(R.id.job_card_map);
 
             itemView.setOnClickListener(new View.OnClickListener()
             {
@@ -68,6 +137,25 @@ public class JobCardAdapter extends FirestoreRecyclerAdapter<JobCard, JobCardAda
                     }
                 }
             });
+
+            // map stuff
+            if (mapView != null)
+            {
+                mapView.onCreate(null);
+                mapView.onResume();
+                mapView.getMapAsync(this);
+            }
+        }
+
+        @Override
+        public void onMapReady(GoogleMap googleMap)
+        {
+            MapsInitializer.initialize(mapView.getContext());
+            tripMap = googleMap;
+
+
+
+            //tripMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(startLat, startLong), 10));
         }
     }
 
